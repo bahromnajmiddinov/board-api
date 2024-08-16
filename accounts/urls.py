0320a5156838cda068bbad78fe0a0a276b235d98
bahromnajmiddinov <najmiddinovbahrom402@gmail.com>
2024-08-16 10:18:06 +0500

    Accounts api views added

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    
+    path('', views.UserList.as_view(), name='user-list'),
+    path('<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
 ]
 
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .serializers import CustomUserSerializer 
+from .models import CustomUser
+
+
+class UserList(APIView):
+    def get(self, request):
+        users = CustomUser.objects.all()
+        serializer = CustomUserSerializer(users, many=True)
+        return Response(serializer.data)
+
+
+class UserDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return CustomUser.objects.get(pk=pk)
+        except CustomUser.DoesNotExist:
+            return Response(status=404)
+    
+    def get(self, request, pk):
+        user = self.get_object(pk)
+        serializer = CustomUserSerializer(user)
+        return Response(serializer.data)
+    
+    def put(self, request, pk):
+        user = self.get_object(pk)
+        serializer = CustomUserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+    
+    def patch(self, request, pk):
+        user = self.get_object(pk)
+        serializer = CustomUserSerializer(user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+    
+    def delete(self, request, pk):
+        user = self.get_object(pk)
+        user.delete()
+        return Response(status=204)
