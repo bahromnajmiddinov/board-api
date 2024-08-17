443af8fa64530635ba9ecefd622a7b8dc69a2413
bahromnajmiddinov <najmiddinovbahrom402@gmail.com>
2024-08-17 08:10:55 +0500

    Add board to start and project functionality added

--- boards/urls.py
+++ boards/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     path('<int:pk>/', views.BoardDetailAPIView.as_view(), name='board-detail'),
     path('projects/', views.ProjectAPIView.as_view(), name='project-list'),
     path('projects/<int:pk>/', views.ProjectDetailAPIView.as_view(), name='project-detail'),
+    path('projects/<int:project_id>/<int:board_id>/add/', views.add_board_to_project, name='add-board-to-project'),
+    
+    path('star/<int:board_id>/', views.star_board, name='star-board'),
 ]
 
--- boards/views.py
+++ boards/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.decorators import api_view
 
 from drf_spectacular.utils import extend_schema, OpenApiParameter
 
@@ -18,18 +19,27 @@ class BoardAPIView(APIView):
         parameters=[
             OpenApiParameter(name='type', description='Get public or requested user`s boards,' \
                              'default public, public|private', required=False, type=str),
+            OpenApiParameter(name='starred', description='Get user`s starred boards, true|false', required=False, type=str),
+            OpenApiParameter(name='team_id', description='Get boards of a specific team, team_id', required=False, type=int),
         ],
         responses={200: BoardSerializer(many=True)},
     )
     
     def get(self, request):
         type = request.GET.get('type', 'public')
-        team_id = request.GET.get('teamId')
+        starred = request.GET.get('starred', 'false')
+        team_id = request.GET.get('team_id', None)
         
         if type == 'private':
             boards = Board.objects.filter(participants_in=[request.user])
         elif type == 'public':
             boards = Board.objects.filter(is_public=True)
+        
+        if team_id:
+            boards = boards.filter(participants_in=[request.user], teams__id=team_id)
+        
+        if starred == 'true':
+            boards = boards.filter(participants_in=[request.user], starred_by__user=request.user)
 
         serializer = BoardSerializer(boards, many=True)
         return Response(serializer.data)
@@ -95,4 +105,33 @@ class ProjectAPIView(ListCreateAPIView):
 class ProjectDetailAPIView(RetrieveUpdateDestroyAPIView):
     serializer_class = ProjectSerializer
     queryset = Project.objects.all()
+
+
+@api_view(['POST'])
+def add_board_to_project(request, project_id, board_id):
+    project = Project.objects.get(pk=project_id, owner=request.request.user)
+    board = Board.objects.get(pk=board_id, created_by=request.request.user)
+    
+    if board not in project.boards.all():
+        project.boards.add(board)
+        return Response(status=200)
+    elif board in project.boards.all():
+        project.boards.remove(board)
+        return Response(status=200)
+    
+    return Response(status=400)
+
+
+@api_view(['POST'])
+def star_board(request, board_id):
+    board = Board.objects.get(pk=board_id, participants_in=[request.request.user])
+    
+    if board.starred_by.filter(user=request.request.user).exists():
+        board.starred_by.remove(request.request.user)
+    else:
+        board.starred_by.add(request.request.user)
+        board.save()
+    
+    return Response(status=200)
+
     
